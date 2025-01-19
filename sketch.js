644cdad6b1a08f3fcfa9be6cc8bf2790d8e17250
Renee Radix <renee.radix@gmail.com>
2025-01-19 14:24:58 -0800

    first commit

---
+++ sketch.js
@@ -0,0 +1,62 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+  strokeWeight(5);
+  stroke(255);
+  noFill();
+  beginShape();
+  vertex(0, height/3)
+  quadraticVertex(width/2, 650, width, height/3);
+  endShape();
+
+  strokeWeight(3);
+  push();
+  rotate(radians(45));
+  rect(100, -50, 100, 100);
+  pop();
+
+  strokeWeight(.5);
+  fill(255);
+  triangle(4, 38, 9, 36, 35, 65);
+  fill(200);
+  triangle(28, 32, 24, 37, 52, 59);
+  triangle(53, 18, 57, 14, 81, 39);
+  triangle(33, 21, 37, 17, 61, 46);
+
+  beginShape();
+  vertex(125, 31);
+
+
+
+  print("Mouse X: " + mouseX);
+  print("Mouse Y: " + mouseY);
+
+  push();
+  translate(110, 100);
+  fill(100);
+  noStroke();
+  ellipse(-5, 5, 75);
+  fill(255);
+  ellipse(-5, 5, 50);
+  rotate(radians(45));
+  fill(214, 122, 158);
+  beginShape();
+  let x = 0;
+  let y = 0;
+  let size = 20;
+  vertex(x, y);
+  bezierVertex(x - size / 2, y - size / 2, x - size, y + size / 3, x, y + size);
+  bezierVertex(x + size, y + size / 3, x + size / 2, y - size / 2, x, y);
+  endShape(CLOSE);
+  pop();
+
+
+
+  // Circle cut up into 16 equidistant points, the inner points of the
+  // logo are tracing a different circle with 8 equal points around
+  // So I'd want to write code that stores the points around a circle so I can make a triangle with them
+
+}
