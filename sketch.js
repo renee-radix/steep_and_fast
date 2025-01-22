25f1f33987a9c2b6b5cb0b375bb050fb0c5ab8fa
Renee Radix <renee.radix@gmail.com>
2025-01-21 17:14:54 -0800

    added more speed lines and spikes at the bottom

--- sketch.js
+++ sketch.js
@@ -14,7 +14,7 @@ function draw() {
 
   fill(0);
   noStroke();
-  ellipse(200, 375, 110, 50);
+  ellipse(200, 375, 113, 50);
 
   strokeWeight(3);
   push();
@@ -30,18 +30,29 @@ function draw() {
   triangle(53, 18, 57, 14, 81, 39);
   triangle(33, 21, 37, 17, 61, 46);
   triangle(69, 13, 73, 8, 90, 31);
+  triangle(124, 27, 126, 24, 141, 46);
+  triangle(150, 60, 154, 56, 169, 77);
+  triangle(31, 124, 35, 119, 49, 140);
+
 
   strokeWeight(3);
-  stroke(255);
+  stroke(250);
 
   beginShape();
-  noFill();
-  vertex(146, 373);
+  fill(255);
+  vertex(146, 375);
   vertex(155, 346);
   vertex(165, 361);
-
-
-
+  vertex(172, 332);
+  vertex(185, 356);
+  vertex(195, 331);
+  vertex(207, 354);
+  vertex(220, 340);
+  vertex(223, 358);
+  vertex(237, 347);
+  vertex(241, 367);
+  vertex(252, 351);
+  vertex(255, 375);
   endShape();
 
 
@@ -72,5 +83,7 @@ function draw() {
   // Circle cut up into 16 equidistant points, the inner points of the
   // logo are tracing a different circle with 8 equal points around
   // So I'd want to write code that stores the points around a circle so I can make a triangle with them
+
+  // If that idea is too complicated just make a spiky circle in the other corner and call it good
 
 }
